0c43ead1e2a56cf7102781e6509a4e72f41fa340
kumarsachinguri <kumarsachinguri@gmail.com>
2024-04-26 13:51:48 +0530

    Header changes

--- ui/src/app/layout/header/header.component.ts
+++ ui/src/app/layout/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
