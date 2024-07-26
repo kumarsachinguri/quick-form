9c3d5bb5442287d0d42954bf3b550a1da0d92634
kumarsachinguri <kumarsachinguri@gmail.com>
2024-07-26 22:58:04 +0530

    create a fresh project for UI

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,16 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    provideAnimationsAsync(),
-    provideAnimationsAsync(),
-    provideAnimationsAsync(),
-    provideAnimationsAsync()
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { FormComponent } from './pages/form/form.component';
 
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard'},
-  { path: 'dashboard', component: DashboardComponent, title: 'Quick Form - Dashboard'},
-  { path: 'form', component: FormComponent, title: 'Quick Form - Form'}
-];
+export const routes: Routes = [];
